81a9d23a5e35644cde3471c37f2027f3cb424480
Bingo <faaa@live.com>
2022-02-04 11:03:28 +0800

    init

---
+++ chapter10/seq.py
@@ -0,0 +1,15 @@
+# seq.py
+
+shopList = ['apple', 'mango', 'carrot', 'banana']
+name = 'swaroop'
+
+# 索引或“订阅”操作
+print('Item 0 is: ', shopList[0])
+print('Item 2 is: ', shopList[2])
+print('Item -1 is: ', shopList[-1])
+
+# Slicing on a list
+print('Item 1 to 3 is: ', shopList[1:3])
+print('Item 2 to end is: ', shopList[2:])
+print('')
+
